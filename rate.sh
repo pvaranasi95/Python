9123db9944c6715f3add40a470de162e7ee0b0ff
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-06-04 13:57:36 +0530

    Update rate.sh

--- rate.sh
+++ rate.sh
@@ -1,3 +1,3 @@
 RATE=$(curl -L -s "https://api.frankfurter.app/latest?from=$From&to=$To" | grep -o '"rates":{"$To":[0-9.]*' | cut -d: -f3 )
 
-echo " 1 $From = " $RATE "$To"
+echo " 1 $From =  $RATE $To"
