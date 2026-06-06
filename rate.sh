2897b71a4c9014e15a7ed9e242ad868d98c404a0
pvaranasi95 <98229537+pvaranasi95@users.noreply.github.com>
2026-06-06 13:21:59 +0530

    Update rate.sh

--- rate.sh
+++ rate.sh
@@ -2,9 +2,6 @@ RATE=$(curl -L -s "https://api.frankfurter.app/latest?from=$From&to=$To" | grep 
 
 echo "1 $From = $RATE $To"
 
-Amount=1000
-RATE=23.572
-
 Final=$(awk "BEGIN {print $Amount * $RATE}")
 
 echo "For $Amount $From You Get $Final $To"
